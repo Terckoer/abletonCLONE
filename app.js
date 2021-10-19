617547c7e8340fa8df7029eb0a8c31261d045506
Terckoer <pv17021353@vallarta.tecmm.edu.mx>
2021-10-19 18:32:44 -0500

    Finished version with details to improve

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-addEventListener('DOMContentLoaded', () =>{
-    const btnNavbar = document.querySelector(".btn-navbar");
-    if(btnNavbar){
-        btnNavbar.addEventListener('click', ()=>{
-            const navbar_menus = document.querySelector('.navbar-menus');
-            navbar_menus.classList.toggle('show');
-            const color = document.querySelector('.menu');
-            color.classList.toggle('color');
-        });
-        console.log("Gaaaaaa")
-    }
+
+
+addEventListener("DOMContentLoaded", () => {
+  const $btnNavbar = document.querySelector(".btn-navbar");
+
+  if ($btnNavbar) {
+    $btnNavbar.addEventListener("click", () => {
+      const $navbar_menus = document.querySelector(".navbar-menus");
+      const $color = document.querySelector('.menu')
+      $navbar_menus.classList.toggle("show");
+      $color.classList.toggle("color");
+    });
+  }
 });
+
+
